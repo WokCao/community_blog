8584d58869db780058283a1fb8732ec9dd34faa2
WokCao <henryquoccao@gmail.com>
2025-10-14 15:35:26 +0700

    style: change UI of post details; update: post action

--- src/main/resources/static/js/sidebar.js
+++ src/main/resources/static/js/sidebar.js
@@ -2,9 +2,12 @@ const menuBtn = document.getElementById("menu-btn");
 const sidebar = document.getElementById("sidebar");
 const closeBtn = document.getElementById("close-btn");
 
-menuBtn.addEventListener("click", () => {
-    sidebar.classList.remove("-translate-x-full");
-});
-closeBtn.addEventListener("click", () => {
-    sidebar.classList.add("-translate-x-full");
-});
+if (menuBtn && closeBtn) {
+    menuBtn.addEventListener("click", () => {
+        sidebar.classList.remove("-translate-x-full");
+    });
+    closeBtn.addEventListener("click", () => {
+        sidebar.classList.add("-translate-x-full");
+    });
+}
+
